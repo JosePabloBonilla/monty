358c4781869df83adde2afe31551b5aebdaed01c
yatoro667 <ytorres667@gmail.com>
2021-05-13 23:18:38 +0000

    Adding functions2

---
+++ functions2.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: voided
+ * @line_number: voided
+ */
+
+void nop(stack_t **stack, unsigned int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
